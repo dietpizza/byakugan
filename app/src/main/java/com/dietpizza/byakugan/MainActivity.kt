7caaef48ea80fbf5d0dc73839539c8f1b1fa1f9e
Rohan Biswas <imrohan550@gmail.com>
2025-11-15 00:47:51 +0530

    added  material3

--- app/src/main/java/com/dietpizza/byakugan/MainActivity.kt
+++ app/src/main/java/com/dietpizza/byakugan/MainActivity.kt
@@ -2,10 +2,15 @@ package com.dietpizza.byakugan
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.color.DynamicColors
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Enable Material 3 dynamic colors (Android 12+)
+        DynamicColors.applyToActivityIfAvailable(this)
+
         setContentView(R.layout.activity_main)
     }
 }
