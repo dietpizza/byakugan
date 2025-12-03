f846e9faca80334c74f3c5c134d186b1398ac5ba
Rohan Biswas <rohan@southguild.tech>
2025-12-03 22:54:18 +0530

    sort dialog revamp

--- app/src/main/java/com/dietpizza/byakugan/components/library/SortSettingsDialog.kt
+++ app/src/main/java/com/dietpizza/byakugan/components/library/SortSettingsDialog.kt
@@ -1,29 +1,28 @@
 package com.dietpizza.byakugan.components.library
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonGroupDefaults
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.dietpizza.byakugan.models.SortBy
@@ -39,32 +38,23 @@ fun SortSettingsDialog(
 ) {
     var selectedSortBy by remember { mutableStateOf(currentSettings.sortBy) }
     var selectedSortOrder by remember { mutableStateOf(currentSettings.sortOrder) }
-    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+
+    fun onSelectValue(sortBy: SortBy = selectedSortBy, sortOrder: SortOrder = selectedSortOrder) {
+
+        selectedSortBy = sortBy
+        selectedSortOrder = sortOrder
+        onConfirm(SortSettings(selectedSortBy, selectedSortOrder))
+        onDismiss()
+    }
+
 
     AlertDialog(
         onDismissRequest = onDismiss,
-        confirmButton = {
-            Button(
-                shape = ButtonGroupDefaults.connectedTrailingButtonShape,
-                onClick = {
-                    onConfirm(SortSettings(selectedSortBy, selectedSortOrder))
-                    onDismiss()
-                }
-            ) {
-                Text("Apply")
-            }
-        },
-        dismissButton = {
-            OutlinedButton(
-                onClick = onDismiss,
-                shape = ButtonGroupDefaults.connectedLeadingButtonShape
-            ) {
-                Text("Cancel")
-            }
-        },
+        confirmButton = {},
+        dismissButton = {},
         title = {
             Text(
-                text = "Settings",
+                text = "Library Settings",
                 style = MaterialTheme.typography.headlineSmall,
                 modifier = Modifier.padding(bottom = 16.dp)
             )
@@ -72,77 +62,119 @@ fun SortSettingsDialog(
 
         text = {
             Column {
-
-                Text(
-                    text = "Sort By",
-                    style = MaterialTheme.typography.labelLarge,
-                    color = MaterialTheme.colorScheme.primary
-                )
-                Spacer(modifier = Modifier.height(4.dp))
-
-                // Sort By options
                 SortOption(
                     label = "Name of file",
                     selected = selectedSortBy == SortBy.NAME,
-                    onClick = { selectedSortBy = SortBy.NAME }
+                    onClick = { onSelectValue(sortBy = SortBy.NAME) },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = VerticalConnectedButtonsShape.TopButtonShape
                 )
                 SortOption(
                     label = "Number of Pages",
                     selected = selectedSortBy == SortBy.PAGES,
-                    onClick = { selectedSortBy = SortBy.PAGES }
+                    onClick = { onSelectValue(sortBy = SortBy.PAGES) },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = VerticalConnectedButtonsShape.MiddleButtonShape
                 )
                 SortOption(
                     label = "Last Modified",
                     selected = selectedSortBy == SortBy.TIME,
-                    onClick = { selectedSortBy = SortBy.TIME }
+                    onClick = { onSelectValue(sortBy = SortBy.TIME) },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = VerticalConnectedButtonsShape.BottomButtonShape
                 )
 
                 Spacer(modifier = Modifier.height(12.dp))
                 HorizontalDivider(thickness = 0.5.dp)
                 Spacer(modifier = Modifier.height(12.dp))
 
-                Text(
-                    text = "Order By",
-                    style = MaterialTheme.typography.labelLarge,
-                    color = MaterialTheme.colorScheme.primary
-                )
-                Spacer(modifier = Modifier.height(6.dp))
-
-                SortOption(
-                    label = "Ascending",
-                    selected = selectedSortOrder == SortOrder.ASCENDING,
-                    onClick = { selectedSortOrder = SortOrder.ASCENDING }
-                )
-                SortOption(
-                    label = "Descending",
-                    selected = selectedSortOrder == SortOrder.DESCENDING,
-                    onClick = { selectedSortOrder = SortOrder.DESCENDING }
-                )
+                Row(
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    SortOption(
+                        label = "Ascending",
+                        selected = selectedSortOrder == SortOrder.ASCENDING,
+                        onClick = { onSelectValue(sortOrder = SortOrder.ASCENDING) },
+                        modifier = Modifier.weight(1f),
+                        shape = HorizontalConnectedButtonsShape.TopButtonShape
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    SortOption(
+                        label = "Descending",
+                        selected = selectedSortOrder == SortOrder.DESCENDING,
+                        onClick = { onSelectValue(sortOrder = SortOrder.DESCENDING) },
+                        modifier = Modifier.weight(1f),
+                        shape = HorizontalConnectedButtonsShape.BottomButtonShape
+                    )
+                }
             }
         }
     )
 }
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 private fun SortOption(
     label: String,
     selected: Boolean,
-    onClick: () -> Unit
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    shape: RoundedCornerShape = RoundedCornerShape(12.dp)
 ) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable(onClick = onClick),
-        verticalAlignment = Alignment.CenterVertically
+
+    val labelColor = if (selected) {
+        MaterialTheme.colorScheme.primary
+    } else {
+        MaterialTheme.colorScheme.onPrimaryContainer
+    }
+
+    Button(
+        onClick,
+        modifier,
+        shape = shape,
+        contentPadding = PaddingValues(12.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = labelColor
+        )
     ) {
-        RadioButton(
-            selected = selected,
-            onClick = onClick
-        )
-        Text(
-            text = label,
-            style = MaterialTheme.typography.bodyMedium
-        )
+        Text(label)
     }
 }
 
+
+object VerticalConnectedButtonsShape {
+    val TopButtonShape = RoundedCornerShape(
+        topStart = 12.dp,
+        bottomStart = 4.dp,
+        topEnd = 12.dp,
+        bottomEnd = 4.dp,
+    )
+    val MiddleButtonShape = RoundedCornerShape(
+        4.dp
+    )
+    val BottomButtonShape = RoundedCornerShape(
+        topStart = 4.dp,
+        bottomStart = 12.dp,
+        topEnd = 4.dp,
+        bottomEnd = 12.dp,
+    )
+}
+
+object HorizontalConnectedButtonsShape {
+    val TopButtonShape = RoundedCornerShape(
+        topStart = 12.dp,
+        bottomStart = 12.dp,
+        topEnd = 4.dp,
+        bottomEnd = 4.dp,
+    )
+    val MiddleButtonShape = RoundedCornerShape(
+        4.dp
+    )
+    val BottomButtonShape = RoundedCornerShape(
+        topStart = 4.dp,
+        bottomStart = 4.dp,
+        topEnd = 12.dp,
+        bottomEnd = 12.dp,
+    )
+}
