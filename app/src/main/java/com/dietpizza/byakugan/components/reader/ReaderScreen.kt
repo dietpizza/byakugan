704180736656a57d3e2a3ec99a95a43fcd3a9b84
Rohan Biswas <rohan@southguild.tech>
2026-05-01 18:32:17 +0530

    nice 4

--- app/src/main/java/com/dietpizza/byakugan/components/reader/ReaderScreen.kt
+++ app/src/main/java/com/dietpizza/byakugan/components/reader/ReaderScreen.kt
@@ -2,7 +2,6 @@ package com.dietpizza.byakugan.components.reader
 
 import android.content.Context
 import android.util.Log
-import androidx.activity.ComponentActivity
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.slideIn
 import androidx.compose.animation.slideOut
@@ -33,7 +32,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
@@ -67,11 +65,9 @@ fun ReaderScreen(
     var parsingProgress by remember { mutableFloatStateOf(0f) }
     var isParsingFile by remember { mutableStateOf(false) }
     var isFileError by remember { mutableStateOf(false) }
-    var mode by remember { mutableStateOf<ReaderModes>(ReaderModes.HORIZONTAL) }
+    var mode by remember { mutableStateOf(ReaderModes.HORIZONTAL) }
     var currentPage by remember { mutableIntStateOf(manga?.lastPage ?: 0) }
     var panelScale by remember { mutableFloatStateOf(1f) }
-
-    val activity = (LocalContext.current as? ComponentActivity)
 
     val pagerState = rememberPagerState(
         initialPage = manga?.lastPage ?: 0,
@@ -87,7 +83,7 @@ fun ReaderScreen(
 
     LaunchedEffect(panels) {
         lifecycleScope.launch {
-            var isFileExist = MangaPanelService.checkExists(manga!!, context)
+            val isFileExist = MangaPanelService.checkExists(manga!!, context)
 
             if (!isFileExist) {
                 Log.e(TAG, "Manga file does not exist: ${manga!!.path}")
