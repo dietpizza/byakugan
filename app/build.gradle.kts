b14b78bf094e8bb57c3aa882aa277d10133f939c
Rohan Biswas <rohan@southguild.tech>
2026-05-07 19:09:53 +0530

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.dietpizza.byakugan"
         minSdk = 33
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.3.0"
+        versionCode = 6
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
